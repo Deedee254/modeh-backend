0633e02f7d0704fc98b8cec4b8651047c4d0e784
mtaandaodigital <mtaandaodigital@gmail.com>
2026-03-24 16:49:34 +0300

    Broadcasting fix

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -2,7 +2,7 @@
 
 return [
 
-    'default' => env('BROADCAST_DRIVER', 'null'),
+    'default' => env('BROADCAST_CONNECTION', env('BROADCAST_DRIVER', 'null')),
 
     'connections' => [
 
@@ -49,3 +49,4 @@ return [
     ],
 
 ];
+
